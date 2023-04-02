686f00a9522da9f6148246853c2338d94e9893c1
les-lentilles <122527407+les-lentilles@users.noreply.github.com>
2023-04-02 14:49:58 +0300

    Delete task10 directory

--- task10/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-Console.Clear();
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"число '{number}'");
-int result = number / 10 % 10;
-Console.WriteLine($"Вторая цифра '{result}'");
- 
